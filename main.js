ee75cfcbd86ce1f4c4c80f925d75b91912669672
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-26 19:20:09 -0300

    Criação do footer

--- main.js
+++ main.js
@@ -17,16 +17,16 @@ for (const link of links) {
 }
 
 // Adicionar classe ao header no momento em que for dado scroll na pagina
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header');
+  const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
   } else {
     header.classList.remove('scroll');
   }
-});
+}
 
 /* Testimonials carousel slider - swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -38,11 +38,11 @@ const swiper = new Swiper('.swiper-container', {
   keyboard: true
 });
 
-/* ScrollReveal: Mostrar elementos gradativamente ao rolar a pagina*/
+// ScrollReveal: Mostrar elementos gradativamente ao rolar a pagina
 const scrollReveal = new ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 1000,
+  duration: 700,
   reset: true
 });
 
@@ -51,7 +51,24 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
-  { interval: 200 }
+  { interval: 150 }
 );
+
+// Botão de voltar para o topo
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top');
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+}
+
+// Listener scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll();
+  backToTop();
+});
