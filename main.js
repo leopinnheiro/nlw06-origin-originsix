1fbf3b7d7a947239b8faec99a25b6d6e7e2c9f28
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-26 21:23:56 -0300

    Ajustes para exibição de seção ativa.

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ for (const link of links) {
 }
 
 // Adicionar classe ao header no momento em que for dado scroll na pagina
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header');
-  const navHeight = header.offsetHeight;
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
   } else {
@@ -64,8 +64,8 @@ scrollReveal.reveal(
 );
 
 // Botão de voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top');
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top');
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show');
   } else {
@@ -73,8 +73,34 @@ function backToTop() {
   }
 }
 
+/*Menu ativo conforme seção visivel na página*/
+const sections = document.querySelectorAll('main section[id]');
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    const currentSection = document.querySelector(
+      `nav ul li a[href*=${sectionId}]`
+    );
+
+    if (checkpointStart && checkpointEnd) {
+      currentSection.classList.add('active');
+    } else {
+      currentSection.classList.remove('active');
+    }
+  }
+}
+
 // Listener scroll
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll();
   backToTop();
+  activateMenuAtCurrentSection();
 });
